1dad24c19f502958151225a12f8ef536508522d0
Aleksei Sotskov <sotskcov.aleksei@yandex.by>
2023-08-30 13:30:12 +0300

    add matrix build description

--- neuralNetwork/NeuralNetwork.py
+++ neuralNetwork/NeuralNetwork.py
@@ -1,4 +1,9 @@
 class NeuralNetwork:
+    # W incoming hidden = hiddenNodes x inputNodes (cause of matrix multiply: row to column)
+    # [A] x [B] - A (row length), B (column length)
+    # cause of this, first matrix build by rule: hiddenNodes x inputNodes
+    # other matrixes are build by rule:
+    # outputNodes x hiddenNodes
     def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate) -> None:
         self.inputNodes = inputNodes
         self.hiddenNodes = hiddenNodes
