c4a95f72430fe881987b0a717218e0ff3717fc59
Aleksei Sotskov <sotskcov.aleksei@yandex.by>
2023-08-30 13:08:05 +0300

    implemented neural network skeleton

---
+++ neuralNetwork/NeuralNetwork.py
@@ -0,0 +1,13 @@
+class NeuralNetwork:
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate) -> None:
+        self.inputNodes = inputNodes
+        self.hiddenNodes = hiddenNodes
+        self.outputNodes = outputNodes
+        self.learningRate = learningRate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
---
+++ neuralNetwork/test.py
@@ -0,0 +1,8 @@
+import NeuralNetwork
+# neural network test script
+inputNodes = 3
+hiddenNodes = 3
+outputNodes = 3
+learningRate = 0.3
+
+testNetwork = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
